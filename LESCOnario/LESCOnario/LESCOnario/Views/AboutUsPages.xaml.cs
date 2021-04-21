086c688a146a217db81a32e9482586f7a54f5be4
HerrodDj <djenane114@gmail.com>
2021-04-20 22:17:28 -0600

    back up

--- LESCOnario/LESCOnario/LESCOnario/Views/AboutUsPages.xaml.cs
+++ LESCOnario/LESCOnario/LESCOnario/Views/AboutUsPages.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using LESCOnario.ViewModels;
 using Xamarin.Forms;
 
 namespace LESCOnario.Views
@@ -15,6 +15,11 @@ namespace LESCOnario.Views
         private async void history_button(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new LescoHistoryPage());
+        }
+
+        private async void ley_button(object sender, EventArgs e)
+        {
+            DependencyService.Get<IPdfViewer>().Open("https://aka.ms/xamebook");
         }
     }
 }
