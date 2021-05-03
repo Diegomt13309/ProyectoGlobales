46a17e8283e38577bb21ab9e2ebf8a0c2ac584fd
JUAN MADRIGAL VASQUEZ <juan.madrigal.vasquez@est.una.ac.cr>
2021-05-03 15:57:41 -0600

    Advance #2 Video Carousel

---
+++ LESCOnario/LESCOnario/LESCOnario/Models/Video.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LESCOnario.Models
+{
+    public class Video
+    {
+       [JsonProperty("Name")]
+       public string name { get; set; }
+
+    }
+}
--- LESCOnario/LESCOnario/LESCOnario/Views/VideoPage.xaml.cs
+++ LESCOnario/LESCOnario/LESCOnario/Views/VideoPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using LESCOnario.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +15,14 @@ namespace LESCOnario.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VideoPage : ContentPage
     {
+        private const string videoUrl = "https://montemagno.com/monkeys.json";
+        private readonly HttpClient httpClient = new HttpClient();
+
+        public ObservableCollection<Video> Videos {get;set;} = new ObservableCollection<Video>();
         public VideoPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
     }
 }
